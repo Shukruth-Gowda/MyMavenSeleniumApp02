cb2e29f75b08cd9ec969711bcc2767259569e1ff
Shukruth-Gowda <gshukruth@gmail.com>
2026-04-13 09:56:08 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,24 +3,28 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App {
-    public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver();
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");       
+        options.addArguments("--no-sandbox");         
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080"); 
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://practicetestautomation.com/practice-test-login/");
+        try { Thread.sleep(5000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
         driver.manage().window().maximize();
-        
-        Thread.sleep(5000);
-
         driver.findElement(By.id("username")).sendKeys("student");
-        Thread.sleep(5000);
+        try { Thread.sleep(5000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
         driver.findElement(By.id("password")).sendKeys("Password123");
-        Thread.sleep(5000);
+        try { Thread.sleep(5000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
         driver.findElement(By.id("submit")).click();
-
-        Thread.sleep(5000);
-
-        driver.quit();
+        try { Thread.sleep(5000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
     }
 }
